4a05628dc2486e0b7ab0e0fabb20a95cfaf0f25e
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-03-14 19:48:20 +0000

    Rename data from csv for clarity

--- tests/app_test.py
+++ tests/app_test.py
@@ -9,16 +9,16 @@ class TestApp:
         return datetime.strptime(datetime_str, "%Y/%m/%d %H:%M:%S %p %Z")
 
     @pytest.fixture()
-    def data_table(self):
+    def survey_responses(self):
         return pandas.read_csv("data/ai_at_made_tech_survey.csv")
 
     @pytest.mark.mandatory
-    def test_timestamp_column_exists(self, data_table):
-        datatest.validate.superset(data_table.columns, {"Timestamp"})
+    def test_timestamp_column_exists(self, survey_responses):
+        datatest.validate.superset(survey_responses.columns, {"Timestamp"})
 
     @pytest.mark.mandatory
-    def test_timestamp_column_type_is_date(self, data_table):
-        timestamp_column = data_table["Timestamp"]
+    def test_timestamp_column_type_is_date(self, survey_responses):
+        timestamp_column = survey_responses["Timestamp"]
         timestamp_column_dates = map(self.__str_to_date, timestamp_column)
 
         datatest.validate(timestamp_column_dates, datetime)
