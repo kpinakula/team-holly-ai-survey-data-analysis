c1520f352119921febc85101ae1396f3a2648614
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-03-14 16:17:46 +0000

    Check first column contains dates

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,8 +1,12 @@
 import pytest
+from datetime import datetime
 import datatest
 import pandas
 
+
 class TestApp:
+    def __str_to_date(self, datetime_str):
+        return datetime.strptime(datetime_str, "%Y/%m/%d %H:%M:%S %p %Z")
 
     @pytest.fixture()
     def data_table(self):
@@ -10,7 +14,11 @@ class TestApp:
 
     @pytest.mark.mandatory
     def test_timestamp_column_exists(self, data_table):
-        datatest.validate.superset(
-            data_table.columns,
-            {'Timestamp'}
-        )
+        datatest.validate.superset(data_table.columns, {"Timestamp"})
+
+    @pytest.mark.mandatory
+    def test_timestamp_column_type_is_date(self, data_table):
+        timestamp_column = data_table["Timestamp"]
+        timestamp_column_dates = map(self.__str_to_date, timestamp_column)
+
+        datatest.validate(timestamp_column_dates, datetime)
