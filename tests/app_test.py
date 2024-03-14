45c936b8c76817b435bb1f32a27be909c0159979
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-03-14 21:08:31 +0000

    Experiment with plotting

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,6 +2,7 @@ import pytest
 from datetime import datetime
 import datatest
 import pandas
+import matplotlib.pyplot as pyplot
 
 
 class TestApp:
@@ -11,6 +12,8 @@ class TestApp:
             "data/ai_at_made_tech_survey.csv",
             parse_dates=["Timestamp"],
             date_format="%Y/%m/%d %H:%M:%S %p %Z",
+            header=0,
+            names=["Timestamp", "work_area", "ai_tool_use_yes_or_no", "ai_tool_use_why_not", "ai_tools_frequently_used", "ai_tools_effectiveness_rating", "ai_tools_productivity_gains", "ai_tools_output_trust_rating", "ai_tools_challenges_limitations", "anything_else", "interest_in_sessions" ]
         )
 
     @pytest.mark.mandatory
@@ -22,3 +25,28 @@ class TestApp:
         submit_dates = survey_responses["Timestamp"]
         parsed_submit_dates = map(pandas.to_datetime, submit_dates)
         datatest.validate(parsed_submit_dates, datetime)
+
+    @pytest.mark.mandatory
+    def test_work_area_column_contains_strings(self, survey_responses):
+        work_areas = survey_responses["work_area"]
+        datatest.validate(work_areas, str)
+
+    @pytest.mark.mandatory
+    def x_test_ai_tools_effectiveness_rating_column_can_be_plotted(self, survey_responses):
+        effectiveness_ratings = survey_responses["ai_tools_effectiveness_rating"]
+        effectiveness_ratings.plot()
+        pyplot.show()
+        datatest.validate(effectiveness_ratings, int)
+
+    @pytest.mark.mandatory
+    def x_test_ai_tools_effectiveness_rating_column_can_be_plotted(self, survey_responses):
+        effectiveness_ratings = survey_responses["ai_tools_effectiveness_rating"]
+        effectiveness_ratings_bar_chart = survey_responses.groupby(effectiveness_ratings).size().plot.bar()
+
+        effectiveness_ratings_bar_chart.bar_label(effectiveness_ratings_bar_chart.containers[0])
+
+        pyplot.xlabel("How would you rate the effectiveness of AI tools in improving your productivity or efficiency?")
+
+        pyplot.show()
+
+        datatest.validate(effectiveness_ratings, int)
