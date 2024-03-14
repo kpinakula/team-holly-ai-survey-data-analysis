5864797b2e6c79e0dd8cebae1125514591c88ffd
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-03-14 19:55:30 +0000

    Refactor date conversion to use pandas

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,20 +5,20 @@ import pandas
 
 
 class TestApp:
-    def __str_to_date(self, datetime_str):
-        return datetime.strptime(datetime_str, "%Y/%m/%d %H:%M:%S %p %Z")
-
     @pytest.fixture()
     def survey_responses(self):
-        return pandas.read_csv("data/ai_at_made_tech_survey.csv")
+        return pandas.read_csv(
+            "data/ai_at_made_tech_survey.csv",
+            parse_dates=["Timestamp"],
+            date_format="%Y/%m/%d %H:%M:%S %p %Z",
+        )
 
     @pytest.mark.mandatory
     def test_timestamp_column_exists(self, survey_responses):
         datatest.validate.superset(survey_responses.columns, {"Timestamp"})
 
     @pytest.mark.mandatory
-    def test_timestamp_column_type_is_date(self, survey_responses):
-        timestamp_column = survey_responses["Timestamp"]
-        timestamp_column_dates = map(self.__str_to_date, timestamp_column)
-
-        datatest.validate(timestamp_column_dates, datetime)
+    def test_timestamp_column_contains_dates(self, survey_responses):
+        submit_dates = survey_responses["Timestamp"]
+        parsed_submit_dates = map(pandas.to_datetime, submit_dates)
+        datatest.validate(parsed_submit_dates, datetime)
